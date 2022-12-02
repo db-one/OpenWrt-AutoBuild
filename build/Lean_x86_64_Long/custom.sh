7547c9a6203004935ac2b1099109730a3460803c
db-one <20243226+db-one@users.noreply.github.com>
2022-12-02 22:06:30 +0800

    Update

--- build/Lean_x86_64_Long/custom.sh
+++ build/Lean_x86_64_Long/custom.sh
@@ -43,30 +43,11 @@ sed -i 's#interval: 5#interval: 1#g' feeds/luci/applications/luci-app-wrtbwmon/h
 
 # ========================定制部分========================
 
-# 设置旁路由和IPV6模式
-cat >> $ZZZ <<-EOF
-# 设置旁路由模式
-# uci set network.lan.gateway='10.0.0.254'                     # 旁路由设置 IPv4 网关
-# uci set network.lan.dns='223.5.5.5 223.6.6.6'                # 旁路由设置 DNS(多个DNS要用空格分开)
-# uci set network.lan.delegate='0'                             # 去掉LAN口使用内置的 IPv6 管理(若用IPV6请把'0'改'1')
-uci set dhcp.@dnsmasq[0].filter_aaaa='0'                     # 禁止解析 IPv6 DNS记录(若用IPV6请把'1'改'0')
-uci set dhcp.lan.ignore='1'                                  # 旁路由关闭DHCP功能
-uci set dhcp.lan.dhcpv6=''                                   # 路由通告服务-禁用
-uci set dhcp.lan.ra=''                                       # DHCPv6 服务-禁用
-uci set dhcp.lan.ra_management=''                            # DHCPv6 模式-禁用
-# uci delete network.lan.type                                # 旁路由桥接模式-禁用
-
-# 如果有用IPV6的话,可以使用以下命令创建IPV6客户端(LAN口)（去掉全部代码uci前面#号生效）
-uci set network.ipv6=interface
-uci set network.ipv6.proto='dhcpv6'
-uci set network.ipv6.ifname='@lan'
-uci set network.ipv6.reqaddress='try'
-uci set network.ipv6.reqprefix='auto'
-uci set firewall.@zone[0].network='lan ipv6'
-EOF
+# cat >> $ZZZ <<-EOF
+# EOF
 
 # 修改退出命令到最后
-sed -i '/exit 0/d' $ZZZ && echo "exit 0" >> $ZZZ
+# sed -i '/exit 0/d' $ZZZ && echo "exit 0" >> $ZZZ
 
 # =======================================================
 
--- build/scripts/files/etc/uci-defaults/xxx-default-settings.sh
+++ build/scripts/files/etc/uci-defaults/xxx-default-settings.sh
@@ -43,4 +43,26 @@ export EDITOR="/usr/bin/nano"
 ' >> /etc/profile
 fi
 
+# 设置旁路由模式
+# uci set network.lan.gateway='10.0.0.254'                     # 旁路由设置 IPv4 网关
+# uci set network.lan.dns='223.5.5.5 223.6.6.6'                # 旁路由设置 DNS(多个DNS要用空格分开)
+# uci set network.lan.delegate='0'                             # 去掉LAN口使用内置的 IPv6 管理(若用IPV6请把'0'改'1')
+uci set dhcp.@dnsmasq[0].filter_aaaa='0'                     # 禁止解析 IPv6 DNS记录(若用IPV6请把'1'改'0')
+uci set dhcp.lan.ignore='1'                                  # 旁路由关闭DHCP功能
+# uci delete network.lan.type                                # 旁路由桥接模式-禁用
+
+# 旁路IPV6需要全部禁用
+uci set network.lan.ip6assign=''                             # IPV6分配长度-禁用
+uci set dhcp.lan.ra=''                                       # 路由通告服务-禁用
+uci set dhcp.lan.dhcpv6=''                                   # DHCPv6 服务-禁用
+uci set dhcp.lan.ra_management=''                            # DHCPv6 模式-禁用
+
+# 如果有用IPV6的话,可以使用以下命令创建IPV6客户端(LAN口)（去掉全部代码uci前面#号生效）
+uci set network.ipv6=interface
+uci set network.ipv6.proto='dhcpv6'
+uci set network.ipv6.ifname='@lan'
+uci set network.ipv6.reqaddress='try'
+uci set network.ipv6.reqprefix='auto'
+uci set firewall.@zone[0].network='lan ipv6'
+
 exit 0
