f8e5d08d24475b6704d2bd27bb71c37ed9930aed
db-one <20243226+db-one@users.noreply.github.com>
2022-12-10 14:10:29 +0800

    Update

--- build/Lean_x86_64_Long/custom.sh
+++ build/Lean_x86_64_Long/custom.sh
@@ -27,13 +27,13 @@ ZZZ="package/lean/default-settings/files/zzz-default-settings"
 #
 sed -i 's#192.168.1.1#10.0.0.1#g' $NET                                                    # 定制默认IP
 # sed -i 's#OpenWrt#OpenWrt-X86#g' $NET                                                     # 修改默认名称为OpenWrt-X86
-sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' $ZZZ                                             # 取消系统默认密码
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                    # 取消系统默认密码
 sed -i "s/OpenWrt /ONE build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ              # 增加自己个性名称
 # sed -i 's/PATCHVER:=5.4/PATCHVER:=4.19/g' target/linux/x86/Makefile                     # 修改内核版本为4.19
 # sed -i "/uci commit luci/i\uci set luci.main.mediaurlbase=/luci-static/neobird" $ZZZ        # 设置默认主题(如果编译可会自动修改默认主题的，有可能会失效)
 # sed -i 's#localtime  = os.date()#localtime  = os.date("%Y年%m月%d日") .. " " .. translate(os.date("%A")) .. " " .. os.date("%X")#g' package/lean/autocore/files/*/index.htm               # 修改默认时间格式
 
-# =======================================================
+# ●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●● #
 sed -i 's#%D %V, %C#%D %V, %C Lean_x86_64#g' package/base-files/files/etc/banner               # 自定义banner显示
 sed -i 's@list listen_https@# list listen_https@g' package/network/services/uhttpd/files/uhttpd.config               # 停止监听443端口
 # sed -i 's#option commit_interval 24h#option commit_interval 10m#g' feeds/packages/net/nlbwmon/files/nlbwmon.config               # 修改流量统计写入为10分钟
@@ -41,15 +41,85 @@ sed -i 's@list listen_https@# list listen_https@g' package/network/services/uhtt
 # sed -i 's#option database_directory /var/lib/nlbwmon#option database_directory /etc/config/nlbwmon_data#g' feeds/packages/net/nlbwmon/files/nlbwmon.config               # 修改流量统计数据存放默认位置
 sed -i 's#interval: 5#interval: 1#g' feeds/luci/applications/luci-app-wrtbwmon/htdocs/luci-static/wrtbwmon/wrtbwmon.js               # wrtbwmon默认刷新时间更改为1秒
 
-# ========================定制部分========================
+# ●●●●●●●●●●●●●●●●●●●●●●●●定制部分●●●●●●●●●●●●●●●●●●●●●●●● #
 
-# cat >> $ZZZ <<-EOF
-# EOF
+cat >> $ZZZ <<-EOF
+# 设置旁路由模式
+# uci set network.lan.gateway='10.0.0.254'                     # 旁路由设置 IPv4 网关
+# uci set network.lan.dns='223.5.5.5 223.6.6.6'                # 旁路由设置 DNS(多个DNS要用空格分开)
+# uci set network.lan.delegate='0'                             # 去掉LAN口使用内置的 IPv6 管理(若用IPV6请把'0'改'1')
+# uci set dhcp.@dnsmasq[0].filter_aaaa='0'                     # 禁止解析 IPv6 DNS记录(若用IPV6请把'1'改'0')
+# uci set dhcp.lan.ignore='1'                                  # 旁路由关闭DHCP功能
+# uci delete network.lan.type                                # 旁路由桥接模式-禁用
+
+# 旁路IPV6需要全部禁用
+# uci set network.lan.ip6assign=''                             # IPV6分配长度-禁用
+# uci set dhcp.lan.ra=''                                       # 路由通告服务-禁用
+# uci set dhcp.lan.dhcpv6=''                                   # DHCPv6 服务-禁用
+# uci set dhcp.lan.ra_management=''                            # DHCPv6 模式-禁用
+
+# 如果有用IPV6的话,可以使用以下命令创建IPV6客户端(LAN口)（去掉全部代码uci前面#号生效）
+uci set network.ipv6=interface
+uci set network.ipv6.proto='dhcpv6'
+uci set network.ipv6.ifname='@lan'
+uci set network.ipv6.reqaddress='try'
+uci set network.ipv6.reqprefix='auto'
+uci set firewall.@zone[0].network='lan ipv6'
+
+EOF
 
 # 修改退出命令到最后
-# sed -i '/exit 0/d' $ZZZ && echo "exit 0" >> $ZZZ
+sed -i '/exit 0/d' $ZZZ && echo "exit 0" >> $ZZZ
 
-# =======================================================
+# ●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●● #
+
+
+# ●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●● #
+# 下载 OpenClash 内核
+grep "CONFIG_PACKAGE_luci-app-openclash=y" $WORKPATH/$CUSTOM_SH >/dev/null
+if [ $? -eq 0 ]; then
+  echo "正在执行：为OpenClash下载内核"
+  mkdir -p $HOME/clash-core
+  cd $HOME/clash-core
+# 下载Dve内核
+  wget -q https://raw.githubusercontent.com/vernesong/OpenClash/master/core-lateset/dev/clash-linux-amd64.tar.gz
+  if [[ $? -ne 0 ]];then
+    wget -q https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-amd64.tar.gz
+  else
+    echo "OpenClash Dve内核压缩包下载成功，开始解压文件"
+  fi
+  tar -zxvf clash-linux-amd64.tar.gz
+  if [[ -f "$HOME/clash-core/clash" ]]; then
+    mkdir -p $HOME/files/etc/openclash/core
+    mv -f $HOME/clash-core/clash $HOME/files/etc/openclash/core/clash
+    chmod +x $HOME/files/etc/openclash/core/clash
+    echo "OpenClash Dve内核配置成功"
+  else
+    echo "OpenClash Dve内核配置失败"
+  fi
+  rm -rf $HOME/clash-core/clash-linux-amd64.tar.gz
+# 下载Meta内核
+  wget -q https://raw.githubusercontent.com/vernesong/OpenClash/master/core-lateset/meta/clash-linux-amd64.tar.gz
+  if [[ $? -ne 0 ]];then
+    wget -q https://raw.githubusercontent.com/vernesong/OpenClash/master/core-lateset/meta/clash-linux-amd64.tar.gz
+  else
+    echo "OpenClash Meta内核压缩包下载成功，开始解压文件"
+  fi
+  tar -zxvf clash-linux-amd64.tar.gz
+  if [[ -f "$HOME/clash-core/clash" ]]; then
+    mkdir -p $HOME/files/etc/openclash/core
+    mv -f $HOME/clash-core/clash $HOME/files/etc/openclash/core/clash_meta
+    chmod +x $HOME/files/etc/openclash/core/clash_meta
+    echo "OpenClash Meta内核配置成功"
+  else
+    echo "OpenClash Meta内核配置失败"
+  fi
+  rm -rf $HOME/clash-core/clash-linux-amd64.tar.gz
+
+  rm -rf $HOME/clash-core
+fi
+
+# ●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●● #
 
 
 # 创建自定义配置文件
@@ -58,7 +128,7 @@ cd $WORKPATH
 touch ./.config
 
 #
-# ========================固件定制部分========================
+# ●●●●●●●●●●●●●●●●●●●●●●●●固件定制部分●●●●●●●●●●●●●●●●●●●●●●●●
 # 
 
 # 
@@ -71,12 +141,12 @@ touch ./.config
 #
 # 有些插件/选项是默认开启的, 如果想要关闭, 请参照以下示例进行编写:
 # 
-#          =========================================
-#         |  # 取消编译VMware镜像:                    |
-#         |  cat >> .config <<EOF                   |
-#         |  # CONFIG_VMDK_IMAGES is not set        |
-#         |  EOF                                    |
-#          =========================================
+#          ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+#        ■|  # 取消编译VMware镜像:                    |■
+#        ■|  cat >> .config <<EOF                   |■
+#        ■|  # CONFIG_VMDK_IMAGES is not set        |■
+#        ■|  EOF                                    |■
+#          ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
 #
 
 # 
@@ -273,7 +343,6 @@ CONFIG_PACKAGE_openssh-sftp-server=y
 CONFIG_PACKAGE_qemu-ga=y
 CONFIG_PACKAGE_myautocore-x86=y
 EOF
-EOF
 
 # 其他软件包:
 cat >> .config <<EOF
@@ -282,7 +351,7 @@ EOF
 
 
 # 
-# ========================固件定制部分结束========================
+# ●●●●●●●●●●●●●●●●●●●●●●●●固件定制部分结束●●●●●●●●●●●●●●●●●●●●●●●● #
 # 
 
 sed -i 's/^[ \t]*//g' ./.config
