33379beb4c2bd3eac66956e5b5100864e5b3a815
db-one <20243226+db-one@users.noreply.github.com>
2023-05-16 08:42:27 +0800

    Update

--- build/Lean_x86_64_Long/custom.sh
+++ build/Lean_x86_64_Long/custom.sh
@@ -50,10 +50,10 @@ cat >> $ZZZ <<-EOF
 # 设置旁路由模式
 # uci set network.lan.gateway='10.0.0.254'                     # 旁路由设置 IPv4 网关
 uci set network.lan.dns='223.5.5.5 119.29.29.29'            # 旁路由设置 DNS(多个DNS要用空格分开)
-# uci set network.lan.delegate='0'                             # 去掉LAN口使用内置的 IPv6 管理(若用IPV6请把'0'改'1')
-# uci set dhcp.@dnsmasq[0].filter_aaaa='0'                     # 禁止解析 IPv6 DNS记录(若用IPV6请把'1'改'0')
 # uci set dhcp.lan.ignore='1'                                  # 旁路由关闭DHCP功能
 # uci delete network.lan.type                                # 旁路由桥接模式-禁用
+# uci set network.lan.delegate='0'                             # 去掉LAN口使用内置的 IPv6 管理(若用IPV6请把'0'改'1')
+# uci set dhcp.@dnsmasq[0].filter_aaaa='0'                     # 禁止解析 IPv6 DNS记录(若用IPV6请把'1'改'0')
 
 # 旁路IPV6需要全部禁用
 # uci set network.lan.ip6assign=''                             # IPV6分配长度-禁用
@@ -192,7 +192,7 @@ EOF
 # 设置固件大小:
 cat >> .config <<EOF
 CONFIG_TARGET_KERNEL_PARTSIZE=16
-CONFIG_TARGET_ROOTFS_PARTSIZE=260
+CONFIG_TARGET_ROOTFS_PARTSIZE=360
 EOF
 
 # 固件压缩:
