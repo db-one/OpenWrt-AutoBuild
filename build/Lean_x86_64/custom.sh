912ebc69429bacafc74d1c2827f7dc5fd766d1ee
db-one <20243226+db-one@users.noreply.github.com>
2024-02-01 09:59:54 +0800

    Update

--- build/Lean_x86_64/custom.sh
+++ build/Lean_x86_64/custom.sh
@@ -26,7 +26,7 @@ ZZZ="package/lean/default-settings/files/zzz-default-settings"
 # 读取内核版本
 KERNEL_PATCHVER=$(cat target/linux/x86/Makefile|grep KERNEL_PATCHVER | sed 's/^.\{17\}//g')
 KERNEL_TESTING_PATCHVER=$(cat target/linux/x86/Makefile|grep KERNEL_TESTING_PATCHVER | sed 's/^.\{25\}//g')
-if [ $KERNEL_TESTING_PATCHVER -gt $KERNEL_PATCHVER ]; then
+if [[ $KERNEL_TESTING_PATCHVER > $KERNEL_PATCHVER ]]; then
   sed -i "s/$KERNEL_PATCHVER/$KERNEL_TESTING_PATCHVER/g" target/linux/x86/Makefile        # 修改内核版本为最新
   echo "内核版本已更新为 $KERNEL_TESTING_PATCHVER"
 else
