c3c4176620837e297a98bbcffff1c8bd402db870
db-one. <20243226+db-one@users.noreply.github.com>
2025-11-22 20:51:55 +0800

    更新

--- build/Lean_x86_64_Long/custom.sh
+++ build/Lean_x86_64_Long/custom.sh
@@ -249,7 +249,7 @@ EOF
 
 # 第三方插件选择:
 cat >> .config <<EOF
-CONFIG_PACKAGE_luci-app-oaf=y #应用过滤
+CONFIG_PACKAGE_luci-app-oaf=n #应用过滤
 CONFIG_PACKAGE_luci-app-openclash=y #OpenClash客户端
 CONFIG_PACKAGE_luci-app-nikki=y #nikki 客户端
 # CONFIG_PACKAGE_luci-app-serverchan=y #微信推送
--- build/immortalwrt_ax6600/custom.sh
+++ build/immortalwrt_ax6600/custom.sh
@@ -17,6 +17,7 @@ git clone https://github.com/db-one/dbone-packages.git -b 23.05 package/dbone-pa
 # 删除部分默认包
 rm -rf feeds/luci/applications/luci-app-qbittorrent
 rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-attendedsysupgrade
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf package/dbone-packages/passwall/packages/v2ray-geoview
 
